1f911badf9dd28213667b0c7f8fd95c48067c7dc
Michael Sargent <michaeljohnsargent@gmail.com>
2015-07-08 12:31:58 -0400

    Made the following changes: + Add a column to the Line_items table + created migration to combine line items in the up method and separate back out in the down method. + provide a flash notice for invalid cart id + log the invalid cart error to development.log + removed cart_id from permitted list in line_items_controller line_items_params method + Added option to empty items in cart + Modified how cart is displayed

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,18 @@
 class StoreController < ApplicationController
+
+  def increment_counter
+    if session[:counter].nil?
+      session[:counter] = 1
+    else
+      session[:counter] += 1
+    end
+  end
+
   def index
     @products = Product.order(:title)
+    @count = increment_counter
+    @time = Time.now
+    @show_message = "You have been here #{@count} times." if session[:counter] > 5
   end
+
 end
