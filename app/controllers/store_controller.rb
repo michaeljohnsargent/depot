08aa83b711a90bbce8d694550785620c5a1902e2
Michael Sargent <michaeljohnsargent@gmail.com>
2015-07-23 16:23:51 -0400

    Updating Depot project

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,13 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
+
+  def index
+    @products = Product.order(:title)
+    @count = increment_counter
+    @time = Time.now
+    @show_message = "You have been here #{@count} times." if session[:counter] > 5
+  end
 
   def increment_counter
     if session[:counter].nil?
@@ -6,13 +15,6 @@ class StoreController < ApplicationController
     else
       session[:counter] += 1
     end
-  end
-
-  def index
-    @products = Product.order(:title)
-    @count = increment_counter
-    @time = Time.now
-    @show_message = "You have been here #{@count} times." if session[:counter] > 5
   end
 
 end
